b8df0c8f9a57ec19cd9b07cc360e31a341a6053c
Alex <alexgregorieart@gmail.com>
2016-07-12 18:36:07 -0400

    Tightening up
    Made a lot of changes. Everything is just about finalized

--- js/app.js
+++ js/app.js
@@ -1,32 +1,43 @@
-// code for scrolling speed from Josh Parrett's Codepen.io example
-// https://codepen.io/JTParrett/pen/BkDie
+$(document).ready(function(){
+    console.log("It works");
+    var tweetText = "Does the Shadow Caste exist? Dark Age finally answered and you will never believe what they found.";
+    tweetText = 'https://twitter.com/intent/tweet?hashtags=DarkAge,theydontreallyexist,gencon2016&text=' + encodeURIComponent(tweetText);
+    $('.twitter-share-button').attr('href', tweetText);
+    console.log(tweetText);
 
-$.fn.moveIt = function(){
-  var $window = $(window);
-  var instances = [];
 
-  $(this).each(function(){
-    instances.push(new moveItItem($(this)));
-  });
-
-  window.onscroll = function(){
-    var scrollTop = $window.scrollTop();
-    instances.forEach(function(inst){
-      inst.update(scrollTop);
+    $('.ion-social-twitter').click(function(){
     });
-  }
-}
 
-var moveItItem = function(el){
-  this.el = $(el);
-  this.speed = parseInt(this.el.attr('data-scroll-speed'));
-};
+    $('.ion-social-facebook').click(function(){
+    });
 
-moveItItem.prototype.update = function(scrollTop){
-  var pos = scrollTop / this.speed;
-  this.el.css('transform', 'translateY(' + -pos + 'px)');
-};
+    $('.ion-social-instagram').click(function(){
+        alert("Instagram");
+    });
 
-$(function(){
-  $('[data-scroll-speed]').moveIt();
+    // Add smooth scrolling to all links
+    $(".navArrow").on('click', function(event) {
+
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+
+
 });
